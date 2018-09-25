ca3690ce2dd4e1280461540a695f8be0a992c3a8
jona62 <jonathanjamesm66@gmail.com>
2018-09-24 20:54:59 -0400

    First Assignment

---
+++ listDir1/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <cstdlib> // For system()
+
+using namespace std;
+
+void menu_item();
+void systemCalls(int);
+
+int main(){
+	menu_item();
+	return 0;
+}
+
+void menu_item() {
+	int choice;
+	do {
+		cout<<"1) Display your current Directory\n";
+		cout<<"2) Display your Home Directory\n";
+		cout<<"3) List the contents of the current Directory\n";
+		cout<<"4) Exit\n";
+		cin>>choice;
+		cout<<"\n";
+		systemCalls(choice);
+
+	} while(choice != 4);
+}
+
+
+void systemCalls(int choice) {
+	switch(choice) {
+		case 1:
+			system("pwd");
+			cout<<"\n";
+			cout<<"\n";
+			break;
+		case 2:
+			system("echo $HOME");
+			cout<<"\n";
+			cout<<"\n";
+			break;
+		case 3:
+			system("ls -al");
+			cout<<"\n";
+			cout<<"\n";
+			break;
+		case 4:
+			return;
+	}
+}
